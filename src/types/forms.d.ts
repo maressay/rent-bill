67bb4db65db8082cab753a14acb8a0b0464940d8
yasseram <yasseravalosmontero14@gmail.com>
2025-04-12 17:50:33 -0500

    added: begining with implementation of cloud database using supabase

--- src/types/forms.d.ts
+++ src/types/forms.d.ts
@@ -11,11 +11,25 @@ export type InvoiceForm = {
 }
 
 export type ElectricityForm = {
-        electricityAgoMonth: string;
-        consumeAgoMonth: string;
-        electricityActualMonth: string;
-        consumeActualMonth: string;
-        kilowattConsume: string;
-        kilowattPrice: string;
-        priceElectricity: string;
+    electricityAgoMonth: string;
+    consumeAgoMonth: string;
+    electricityActualMonth: string;
+    consumeActualMonth: string;
+    kilowattConsume: string;
+    kilowattPrice: string;
+    priceElectricity: string;
 }
+
+export type Invoice = {
+    customer_name?: string | null;
+    lote: string;
+    apartment_number?: number | null;
+    apartment_rent?: number | null;
+    water_fee?: number | null;
+    electricity_fee?: number | null;
+    cable_fee?: number | null;
+    internet_fee?: number | null;
+    cleaning_fee?: number | null;
+};
+
+
